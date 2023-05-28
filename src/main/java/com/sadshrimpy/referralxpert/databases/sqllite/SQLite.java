076670d9961bdfaf76a9fcc071dd11662fcbef72
SadShrimpy <alessionice@gmail.com>
2023-05-28 13:32:34 +0200

    Rebranch dev

--- src/main/java/com/sadshrimpy/referralxpert/databases/sqllite/SQLite.java
+++ src/main/java/com/sadshrimpy/referralxpert/databases/sqllite/SQLite.java
@@ -2,10 +2,40 @@ package com.sadshrimpy.referralxpert.databases.sqllite;
 
 import com.sadshrimpy.referralxpert.databases.DatabaseSyntax;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLite implements DatabaseSyntax {
+
+    private Connection conn;
 
     @Override
     public boolean create(String name) {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:simple.db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    @Override
+    public byte connect() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlite:simple.db");
+            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+                return 0;
+            }
+            return 1;
+        }
+        return 2;
+    }
+
+    @Override
+    public boolean close() {
         return false;
     }
 
