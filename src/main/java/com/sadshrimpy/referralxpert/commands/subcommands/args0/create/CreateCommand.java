2726c98907c8d38f9a51b80c8ecac9280679d325
SadShrimpy <123626018+SadShrimpyy@users.noreply.github.com>
2023-06-13 18:31:37 +0200

    Player not null

--- src/main/java/com/sadshrimpy/referralxpert/commands/subcommands/args0/create/CreateCommand.java
+++ src/main/java/com/sadshrimpy/referralxpert/commands/subcommands/args0/create/CreateCommand.java
@@ -4,8 +4,12 @@ import com.sadshrimpy.referralxpert.codes.Codes;
 import com.sadshrimpy.referralxpert.commands.CommandSyntax;
 import com.sadshrimpy.referralxpert.utils.sadlibrary.SadChat;
 import com.sadshrimpy.referralxpert.utils.sadlibrary.SadPlaceholders;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 import static com.sadshrimpy.referralxpert.ReferralXpert.sadLibrary;
 
@@ -40,19 +44,30 @@ public class CreateCommand implements CommandSyntax {
     @Override
     public void perform(CommandSender sender) {
 //        /rxp create <code> <maximum-usages> <player>
-        if (sadLibrary.codes().getCodes().containsKey(cmdArgs[1]))
+        if (sadLibrary.codes().getCodes().containsKey(cmdArgs[1])) {
             sender.sendMessage(chat.viaChat(true, msg.getString("referral.creation.cannot-create")
                     .replace(sadLibrary.placeholders().getCode(), cmdArgs[1])));
-        else {
-            SadPlaceholders place = sadLibrary.placeholders();
-            double usages = Double.parseDouble(cmdArgs[2]) <= 0 ? Double.POSITIVE_INFINITY : Double.parseDouble(cmdArgs[2]);
-
-            sender.sendMessage(chat.viaChat(true, msg.getString("referral.creation.created")
-                    .replace(place.getCode(), cmdArgs[1])
-                    .replace(place.getCodeMaxUsages(), Integer.toString((int) usages))
-                    .replace(place.getPlayerName(), cmdArgs[3])));
-
-            sadLibrary.codes().getCodes().put(cmdArgs[1], new Codes(cmdArgs[1], usages));
+            return;
         }
+
+        if (Bukkit.getPlayer(cmdArgs[3]) == null) {
+            sender.sendMessage(chat.viaChat(true, msg.getString("player.not-found")
+                    .replace(sadLibrary.placeholders().getPlayerName(), sender.getName())
+                    .replace(sadLibrary.placeholders().getPlayerInvolved(), cmdArgs[3])));
+            return;
+        }
+
+        Player involved = Bukkit.getPlayer(cmdArgs[3]);
+        assert involved != null;
+
+        SadPlaceholders place = sadLibrary.placeholders();
+        double usages = Double.parseDouble(cmdArgs[2]) <= 0 ? Double.POSITIVE_INFINITY : Double.parseDouble(cmdArgs[2]);
+
+        sender.sendMessage(chat.viaChat(true, msg.getString("referral.creation.created")
+                .replace(place.getCode(), cmdArgs[1])
+                .replace(place.getCodeMaxUsages(), Integer.toString((int) usages))
+                .replace(place.getPlayerName(), cmdArgs[3])));
+
+        sadLibrary.codes().getCodes().put(cmdArgs[1], new Codes(cmdArgs[1], usages, involved.getUniqueId()));
     }
 }
