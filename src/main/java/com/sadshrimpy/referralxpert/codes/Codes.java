98720bd50ff6014adfd01df3028a69a40c25ce22
SadShrimpy <123626018+SadShrimpyy@users.noreply.github.com>
2023-06-13 18:32:35 +0200

    Adding ownerId

--- src/main/java/com/sadshrimpy/referralxpert/codes/Codes.java
+++ src/main/java/com/sadshrimpy/referralxpert/codes/Codes.java
@@ -1,21 +1,28 @@
 package com.sadshrimpy.referralxpert.codes;
 
+import java.util.UUID;
+
 public class Codes {
 
-    private String code;
     private double usages;
+    private String code;
+    private UUID ownerId;
 
-    public Codes(String code, double usages) {
-        this.code = code;
+    public Codes(String code, double usages, UUID ownerId) {
+        this.ownerId = ownerId;
         this.usages = usages;
+        this.code = code;
     }
 
+    public double getUsages() {
+        return usages;
+    }
     /** Getters */
     public String getCode() {
         return code;
     }
-    public double getUsages() {
-        return usages;
+    public UUID getOwnerId() {
+        return ownerId;
     }
 
     /** Setters */
