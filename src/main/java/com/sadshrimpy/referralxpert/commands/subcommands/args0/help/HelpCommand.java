d96724f2fde6124258b5c65c1c0bc6ca8a6fe5ca
SadShrimpy <123626018+SadShrimpyy@users.noreply.github.com>
2023-06-15 00:54:10 +0200

    HashMap for the dynamics-hoverable help list

--- src/main/java/com/sadshrimpy/referralxpert/commands/subcommands/args0/help/HelpCommand.java
+++ src/main/java/com/sadshrimpy/referralxpert/commands/subcommands/args0/help/HelpCommand.java
@@ -3,14 +3,16 @@ package com.sadshrimpy.referralxpert.commands.subcommands.args0.help;
 import com.sadshrimpy.referralxpert.commands.CommandSyntax;
 import com.sadshrimpy.referralxpert.utils.sadlibrary.SadChat;
 import com.sadshrimpy.referralxpert.utils.sadlibrary.SadPlaceholders;
+import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static com.sadshrimpy.referralxpert.ReferralXpert.sadLibrary;
 
@@ -24,16 +26,20 @@ public class HelpCommand implements CommandSyntax {
     private byte pageMax;
     private byte page;
 
+    private Map<String, HoverEvent> heMap = new HashMap<>();
+
     protected FileConfiguration msg = sadLibrary.configurations().getMessages();
+    protected SadChat chat = sadLibrary.chat();
     protected SadPlaceholders place = sadLibrary.placeholders();
 
 
     public HelpCommand(String[] strings) {
         cmdArgs = strings;
+        initHeMap();
     }
 
     public HelpCommand() {
-
+        initHeMap();
     }
 
     @Override
@@ -65,7 +71,6 @@ public class HelpCommand implements CommandSyntax {
     public void perform(CommandSender sender) {
         helpList = msg.getStringList("help.list");
         int sz = helpList.size();
-        SadChat chat = sadLibrary.chat();
 
 /*
   // todo select the amount displayed
@@ -108,28 +113,39 @@ public class HelpCommand implements CommandSyntax {
     /**
      * Print the menu without the space
      * */
-    private void printWithSpace(CommandSender sender, SadChat msgC) {
+    private void printWithSpace(CommandSender sender, SadChat chat) {
 /*
         int remainCmds = cmds.size() - (size * (page - 1));
         if (remainCmds < size) // if remain cmds ...
             finish -= size - remainCmds; // NO 7
         if (finish > cmds.size()) finish = cmds.size();
 */
-        RowPosition row = getRow(sender, msgC);
+        RowPosition row = getRow(sender, chat);
         if (row == null) return;
 
         boolean hPage = this.msg.getBoolean("help.page.enabled");
         TextComponent rowBtns = new ButtonsRow(page, pageMax).getRowHoverable("help.page.row");
         Player player = Bukkit.getPlayer(sender.getName());
 
+        // TODO: 15/06/2023 fix console command sender
+        Player.Spigot spigot = Objects.requireNonNull(player).spigot();
+
         if (hPage && row.isFirst(row))
             if (player != null) {
-                player.spigot().sendMessage(rowBtns);
+                spigot.sendMessage(rowBtns);
                 sender.sendMessage("");
             }
 
+        TextComponent tc = new TextComponent();
         while (start <= finish && helpList.size() > start) {
-            sender.sendMessage(msgC.viaChat(false, helpList.get(start)));
+//            sender.sendMessage(chat.viaChat(false, helpList.get(start)));
+            // TODO: 14/06/2023 create text component and send
+            tc.addExtra(chat.translateColors(helpList.get(start)));
+            tc.setHoverEvent(getHoverable(helpList.get(start)));
+            spigot.sendMessage(tc);
+            tc.setText("");
+            tc.setHoverEvent(null);
+            tc.setExtra(new ArrayList<>(0));
             if (start < finish)
                 sender.sendMessage("");
             start++;
@@ -138,8 +154,18 @@ public class HelpCommand implements CommandSyntax {
         if (hPage && !row.isFirst(row))
             if (player != null) {
                 sender.sendMessage("");
-                player.spigot().sendMessage(rowBtns);
+                spigot.sendMessage(rowBtns);
             }
+    }
+
+    /**
+     * Return the translated hoverable text for the specific command
+     * */
+    private HoverEvent getHoverable(String query) {
+        for (String command : heMap.keySet())
+            if (query.contains(command))
+                return heMap.get(command);
+        return null;
     }
 
     /**
@@ -194,5 +220,26 @@ public class HelpCommand implements CommandSyntax {
         sender.sendMessage(msgC.viaChat(false, msg.getString("help.banner")
                 .replace(place.getHelpCurPage(), Integer.toString(page))
                 .replace(place.getHelpMaxPage(), Integer.toString(pageMax))));
+    }
+
+    /**
+     * Construct the HashMap
+     * */
+    private void initHeMap() {
+        heMap.put("/rxp help", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.help")))));
+        heMap.put("/rxp create", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.create")))));
+        heMap.put("/rxp check", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.check-specific")))));
+        heMap.put("/rxp check-specific", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.check")))));
+        heMap.put("/rxp goals", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.goals")))));
+        heMap.put("/rxp score", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.score")))));
+        heMap.put("/rxp scoreboard", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.scoreboard-gui")))));
+        heMap.put("/rxp scoreboard-gui", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.scoreboard")))));
+        heMap.put("/rxp set", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.set")))));
+        heMap.put("/rxp maximum", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.maximum")))));
+        heMap.put("/rxp redeem", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.redeem")))));
+        heMap.put("/rxp reload", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.reload")))));
+        heMap.put("/rxp reset", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.reset")))));
+        heMap.put("/rxp reward", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.reward")))));
+        heMap.put("/rxp secret", new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(chat.translateColors(msg.getString("help.hovers.commands.secret")))));
     }
 }
