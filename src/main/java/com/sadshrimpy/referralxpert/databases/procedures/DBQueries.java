0e5b7dbf43f213380ef20f632dfa7226dfc046aa
SadShrimpy <123626018+SadShrimpyy@users.noreply.github.com>
2023-06-15 12:59:44 +0200

    Methods reworked
    registerNewPlayer become registerPlayers.
    updateTime became updatePlayersTime.
    
    Methods rewrited and optimized through the help of lists

--- src/main/java/com/sadshrimpy/referralxpert/databases/procedures/DBQueries.java
+++ src/main/java/com/sadshrimpy/referralxpert/databases/procedures/DBQueries.java
@@ -1,6 +1,8 @@
 package com.sadshrimpy.referralxpert.databases.procedures;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class DBQueries {
@@ -39,40 +41,42 @@ public class DBQueries {
     /** Querys to ALTER TABLE(S) */
 
     /**
-     * @implNote Register a player in the database.
-     * @return -2 the exception, 0 if the update didn't success, else 1 if the update success
+     * Update the player's time with the given lists. In case of error, the list will be updated and printed later
      * */
-    public byte registerNewPlayer(UUID uuid, long time) {
-        try {
-            stmt = connection.prepareStatement(new StringBuilder(80)
-                    .append("INSERT INTO player (streak, online_time, uuid, last_code) ")
-                    .append("VALUES ")
-                    .append("(?, ?, ?, ?);").toString());
-            stmt.setString(1, String.valueOf(0));
-            stmt.setString(2, String.valueOf(time));
-            stmt.setString(3, String.valueOf(uuid));
-            stmt.setString(4, "");
-            return (byte) stmt.executeUpdate();
-        } catch (SQLException e) {
-            return -2;
-        }
+    public void updatePlayersTime(List<String> errorMessages, List<UUID> uuidsToUpdate, HashMap<UUID, Long> timeMap) {
+        uuidsToUpdate.forEach(uuid -> {
+            try {
+                stmt = connection.prepareStatement(new StringBuilder(70)
+                        .append("UPDATE player ")
+                        .append("SET online_time = online_time + ? ")
+                        .append("WHERE uuid = ?;").toString());
+                stmt.setString(1, String.valueOf(timeMap.get(uuid)));
+                stmt.setString(2, String.valueOf(uuid));
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                errorMessages.add("Update Player >> " + uuid);
+            }
+        });
     }
 
     /**
-     * @implNote Register a player in the database.
-     * @return -2 the exception, 0 if the update didn't success, else 1 if the update success
+     * Register the players in the database. In case of error, the list will be updated and printed later
      * */
-    public byte updateTime(UUID uuid, Long time) {
-        try {
-            stmt = connection.prepareStatement(new StringBuilder(80)
-                    .append("UPDATE player ")
-                    .append("SET online_time = online_time + ? ")
-                    .append("WHERE uuid = ?;").toString());
-            stmt.setString(1, String.valueOf(time));
-            stmt.setString(2, String.valueOf(uuid));
-            return (byte) stmt.executeUpdate();
-        } catch (SQLException e) {
-            return -2;
-        }
+    public void registerPlayers(List<String> errorMessages, List<UUID> uuidsToRegister, HashMap<UUID, Long> timeMap) {
+        uuidsToRegister.forEach(uuid -> {
+            try {
+                stmt = connection.prepareStatement(new StringBuilder(80)
+                        .append("INSERT INTO player (streak, online_time, uuid, last_code) ")
+                        .append("VALUES ")
+                        .append("(?, ?, ?, ?);").toString());
+                stmt.setString(1, String.valueOf(0));
+                stmt.setString(2, String.valueOf(timeMap.get(uuid)));
+                stmt.setString(3, String.valueOf(uuid));
+                stmt.setString(4, "");
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                errorMessages.add("Register New Player >> " + uuid);
+            }
+        });
     }
 }
